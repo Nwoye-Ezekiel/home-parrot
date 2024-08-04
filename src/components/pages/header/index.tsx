f771d94b9738d946e74cc5cae7e6dd65c6755805
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2024-08-04 07:11:58 +0100

    redesign favicon

--- src/components/pages/header/index.tsx
+++ src/components/pages/header/index.tsx
@@ -18,7 +18,7 @@ export default function Header() {
   return (
     <div className="flex justify-center items-center space-x-5 px-20 absolute top-0 left-0 w-full h-20 z-20">
       <div className="grid grid-cols-10 items-center w-full">
-        <Logo className="w-10" />
+        <Logo className="w-12" />
         <div className="col-span-8 text-center space-x-5">
           {navigationLinks.map((link, index) => (
             <button key={index} onClick={() => scrollToSection(link.name)}>
